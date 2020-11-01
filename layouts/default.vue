9303e24bce755577b1e0fcf49dd42caeeb9afc21
Stevey <60765528+StephenDeVaux@users.noreply.github.com>
2020-11-01 12:24:39 +1100

    Added navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <p>You Tube serach Apps! <span class="badge badge-secondary">New</span></p>
-        <Nuxt />
-    </div>
+  <div>
+    <nav class="navbar sticky-top navbar-light bg-light">
+      <a class="navbar-brand" href="/">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/youtube-85-226402.png"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt=""
+        />
+        YouTube Search
+      </a>
+    </nav>
+    <p>You Tube serach Apps! <span class="badge badge-secondary">New</span></p>
+    <Nuxt />
+  </div>
 </template>
 
 <style>
-
-
 </style>
